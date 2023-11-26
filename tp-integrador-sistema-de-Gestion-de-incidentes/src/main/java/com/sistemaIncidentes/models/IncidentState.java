b7c7b4411e6ceabd21de593d50f6fca3c94b6599
Geronimo Silvestre <126483704+geronimosilvestre@users.noreply.github.com>
2023-11-26 15:47:01 -0300

    Delete tp-integrador-sistema-de-Gestion-de-incidentes directory
    hice push de la carpeta entrera...

--- tp-integrador-sistema-de-Gestion-de-incidentes/src/main/java/com/sistemaIncidentes/models/IncidentState.java
+++
@@ -1,9 +0,0 @@
-package com.sistemaIncidentes.models;
-
-public interface IncidentState {
-
-    public double getTime();
-    public void close(double timeInHours);
-    public void setIncident(Incident incident);
-    public Incident getIncident();
-}
